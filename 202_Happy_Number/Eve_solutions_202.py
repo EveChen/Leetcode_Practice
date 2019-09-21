75099df2afaca01b05d05bdff52c8a3e5d0819f6
Eve <eve@Eves-Air.attlocal.net>
2019-09-20 21:27:00 -0700

    Use set and dictionary to solve this question

---
+++ 202_Happy_Number/Eve_solutions_202.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        # Method 1: Use Set
+        mem = set()
+        while n != 1:
+            n = sum(int(i) ** 2 for i in str(n))
+            # Case 1: If n has already in mem, we returns false to stop the endless cycle
+            if n in mem:
+                return False
+            else:
+                mem.add(n)
+            
+        return True  
+        
+
+        # Method 2: Use Dictionary
+        hashmap = {}
+
+        while n != 1:
+            n = sum(pow(int(i), 2) for i in str(n))
+            if n in hashmap:
+                return False
+            else:
+                hashmap[n] = True # Add a dummy variable to detect endless cycle
+
+        return True
+        
