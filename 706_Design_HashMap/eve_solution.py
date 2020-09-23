56b046833fa65c41fb75fc22658d7b77d01c2cd7
Eve <eve@Eves-Air.attlocal.net>
2020-09-23 09:40:08 -0700

    One question on 2020/09/23

---
+++ 706_Design_HashMap/eve_solution.py
@@ -0,0 +1,37 @@
+
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hashmap = {}
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        self.hashmap[key] = value
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        if key in self.hashmap:
+            return self.hashmap[key]
+        else:
+            return -1
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        if key in self.hashmap:
+            del self.hashmap[key]
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
