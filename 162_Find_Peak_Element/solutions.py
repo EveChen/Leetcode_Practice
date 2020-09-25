82b4dcc68d4cd8aacaf5bb6d911ea45f6aa1c4d3
Eve <eve@Eves-Air.attlocal.net>
2020-09-24 21:09:39 -0700

    Update 162's solution - 9/24/2020

--- 162_Find_Peak_Element/solutions.py
+++ 162_Find_Peak_Element/solutions.py
@@ -19,35 +19,35 @@
 
 
 # Leetcode's solution - iteration
-class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
-        start, end = 0, len(nums) - 1
-        
-        while start < end:
-            mid = start + (end - start) // 2
-            if nums[mid] > nums[mid + 1]:
-                end = mid
-            else:
-                start = mid + 1
-        return end  
-    
-    
-    
-# Leetcode's solution - recursion -- 不知道哪裡錯?
 # class Solution:
-#     def search(nums, start, end):
-#             if start == end:
-#                 return start
-#             mid = start + (end - start) // 2
-#             if nums[mid] > nums[mid + 1]: #此時要把end = mid
-#                 return search(nums, 1, mid)
-#             else: #這時候nums[mid] < nums[mid + 1]，在山峰的左邊，故把start = mid + 1
-#                 return search(nums, mid + 1, end) 
-            
 #     def findPeakElement(self, nums: List[int]) -> int:
-#         return search(nums, 0, len(nums) - 1)
+#         if len(nums) == 1:
+#             return 0
+#         start, end = 0, len(nums) - 1
+        
+#         while start < end:
+#             mid = start + (end - start) // 2
+#             if nums[mid] > nums[mid + 1]:
+#                 end = mid
+#             else:
+#                 start = mid + 1
+#         return end  
+    
+    
+    
+# Leetcode's solution - recursion
+class Solution:
+    def search(self, nums, start, end):
+            if start == end:
+                return start
+            mid = start + (end - start) // 2
+            if nums[mid] > nums[mid + 1]: #此時要把end = mid
+                return self.search(nums, 0, mid)
+            else: #這時候nums[mid] < nums[mid + 1]，在山峰的左邊，故把start = mid + 1
+                return self.search(nums, mid + 1, end) 
+            
+    def findPeakElement(self, nums: List[int]) -> int:
+        return self.search(nums, 0, len(nums) - 1)
     
         
 
