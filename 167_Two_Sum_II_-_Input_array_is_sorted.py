ae2266016cbd6b6932ce6f7d1700fab002b0e3d2
Eve <eve@Eves-Air.attlocal.net>
2021-06-21 23:13:06 +0800

    Update 167 Two Sum II

--- 167_Two_Sum_II_-_Input_array_is_sorted.py
+++ 167_Two_Sum_II_-_Input_array_is_sorted.py
@@ -1,14 +1,27 @@
 
-# link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/ 
 
-# Solution A: hashmap
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i in range(len(numbers)):
-            if target - numbers[i] in hashmap:
-                return [hashmap[target - numbers[i]] + 1, i + 1]
-            hashmap[numbers[i]] = i
+# # Solution A: hashmap
+# class Solution:
+#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#         hashmap = {}
+#         for i in range(len(numbers)):
+#             if target - numbers[i] in hashmap:
+#                 return [hashmap[target - numbers[i]] + 1, i + 1]
+#             hashmap[numbers[i]] = i
             
             
 # Solution B: Binary search
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            left = i
+            right = len(numbers)
+            while left < right:
+                mid = (left + right) // 2
+                if numbers[mid] > target:
+                    right = mid
+                elif numbers[mid] < target:
+                    left = mid
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
