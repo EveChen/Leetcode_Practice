7c8f9b1a40315540c67906a9cd2580ad9d963965
Eve <eve@Eves-Air.attlocal.net>
2021-06-22 09:56:20 +0800

    167 Add two pointer & binary search solutions (still don't understand)

--- 167_Two_Sum_II_-_Input_array_is_sorted.py
+++ 167_Two_Sum_II_-_Input_array_is_sorted.py
@@ -1,6 +1,4 @@
 
-link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/ 
-
 # # Solution A: hashmap
 # class Solution:
 #     def twoSum(self, numbers: List[int], target: int) -> List[int]:
@@ -11,17 +9,37 @@ link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 #             hashmap[numbers[i]] = i
             
             
-# Solution B: Binary search
+# # Solution B: Two pointers
+# class Solution:
+#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#         left = 0
+#         right = len(numbers) - 1
+#         while left < right:
+#             total = numbers[left] + numbers[right]
+#             if total < target:
+#                 left += 1
+#             elif total > target:
+#                 right -= 1
+#             elif total == target:
+#                 return [left + 1, right + 1]
+
+# Solution C: Binary Search
+# 思路: 因為一定有解，而且數組是有序的，那麼第一個數字肯定要小於目標值target，那麼我們每次用二分法來搜索target - numbers[i]即可
+# Status: Wrong answer......
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         for i in range(len(numbers)):
             left = i
-            right = len(numbers)
+            right = len(numbers) - 1
+            tmp = target - numbers[i]
+            
             while left < right:
-                mid = (left + right) // 2
-                if numbers[mid] > target:
-                    right = mid
-                elif numbers[mid] < target:
-                    left = mid
-            if numbers[left] + numbers[right] == target:
-                return [left + 1, right + 1]
+                mid = left + (left - right) // 2
+                if tmp == numbers[mid]: 
+                    return [i + 1, mid + 1]
+                elif tmp > numbers[mid]:
+                    left += 1
+                elif tmp < numbers[mid]:
+                    right -= 1
+                    
+
