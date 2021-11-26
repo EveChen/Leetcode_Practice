8f52973cf7e6e54e30590435912e18b652ec12ca
Eve <yuanyichen1@gmail.com>
2021-11-26 22:25:07 +0800

    Modify Q1082

--- SQL_questions/1082_Sales_Analysis_I.py
+++ SQL_questions/1082_Sales_Analysis_I.py
@@ -1,13 +1,45 @@
 
 # https://leetcode.com/problems/sales-analysis-i/
 
-# Solution - 直接用 Sales table 中的 price
-Select seller_id
+# # Solution A: 直接用 Sales table 中的 price
+# Select seller_id
+# From Sales
+# Group By seller_id
+# Having Sum(price) = (Select Sum(price)
+#                     From Sales
+#                     Group By seller_id
+#                     Order By 1 Desc
+#                     Limit 1)
+
+
+# Solution B: 先做一個 tmp table 再 query
+# With tmp As
+# (Select seller_id, Sum(price) As total_price
+# From Sales
+# Group by seller_id)
+
+# Select seller_id
+# From tmp
+# Where total_price = (Select Max(total_price)
+#                     From tmp)
+
+
+
+
+-- -- Solution C: rank()
+-- select seller_id
+-- from
+--     (select seller_id, 
+--             rank() over(order by sum(price) desc) as rank
+--     from Sales
+--     group by seller_id) as tmp
+-- where rank = 1
+
+
+-- Solution D: Top 1 with ties
+Select Top 1 With Ties seller_id
 From Sales
 Group By seller_id
-Having Sum(price) = (Select Sum(price)
-                    From Sales
-                    Group By seller_id
-                    Order By 1 Desc
-                    Limit 1)
+Order By Sum(price) Desc
+
 
