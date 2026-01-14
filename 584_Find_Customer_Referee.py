a99213a9bf80c2ab42412c27b99d0f8d62ffb3b1
Eve Chen <yuanyichen1@gmail.com>
2026-01-14 11:42:39 +1100

    Move Question 584

--- 584_Find_Customer_Referee.py
+++ 584_Find_Customer_Referee.py
@@ -13,16 +13,3 @@ Where referee_id != 2 Or referee_id is null
 # WHERE COALESCE(referee_id,0) <> 2
 
 
-# Wrong answer
-# Select c1.name
-# From Customer c1
-# Join Customer c2
-# On c1.id = c2.id AND c2.referee_id != 2 
-
-
-# Wrong answer
-# Select name
-# From Customer c1
-# Where referee_id Not In (Select id
-#         From Customer c2
-#         Where id = 2) 
